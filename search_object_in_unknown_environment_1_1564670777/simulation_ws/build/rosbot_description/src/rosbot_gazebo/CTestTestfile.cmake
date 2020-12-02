e47b3fdd5bc5ed7cc8037a0da1ff5db1da0c23ce
hhhtzq <59558378+hhhtzq@users.noreply.github.com>
2020-12-02 15:31:58 +0100

    update

--- search_object_in_unknown_environment_1_1564670777/simulation_ws/build/rosbot_description/src/rosbot_gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/simulation_ws/src/rosbot_description/src/rosbot_gazebo
-# Build directory: /home/user/simulation_ws/build/rosbot_description/src/rosbot_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- search_object_in_unknown_environment_1_1564670777/simulation_ws/build/rosbot_launch/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/simulation_ws/src/rosbot_launch
-# Build directory: /home/user/simulation_ws/build/rosbot_launch
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
